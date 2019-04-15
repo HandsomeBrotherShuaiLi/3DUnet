420d93911e4e0223dfbf7bfac0bd7d53464a9252
chris.li2 <chris.li2@nio.com>
2019-04-15 17:24:15 +0800

    add dataloader

--- data_loader.py
+++ data_loader.py
@@ -2,7 +2,12 @@ import nrrd
 from PIL import Image
 import numpy as np
 import os
-
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+"""
+原始的图片是vol.nrrd，格式是h,w,深度
+标注的信息是label.nrrd,格式也是H,w,深度，每一个具体的点上的值就是该像素属于的类别
+"""
 class data_loader(object):
     def __init__(self,data_dir):
         self.dir=data_dir
@@ -10,10 +15,30 @@ class data_loader(object):
         for i in os.listdir(self.dir):
             path=os.path.join(self.dir,i)
             for i in os.listdir(path):
-                if i.endswith('nrrd'):
+                if i.endswith('vol.nrrd'):
                     filepath=os.path.join(path,i)
                     data,nrrd_options=nrrd.read(filepath)
                     print(i,data.shape)
+                    # for j in nrrd_options:
+                    #     print(j, nrrd_options[j])
+                elif i.endswith('seg.nrrd'):
+                    filepath = os.path.join(path, i)
+                    data, nrrd_options = nrrd.read(filepath)
+                    print(i,data.shape)
+                    for j in nrrd_options:
+                        print(j,nrrd_options[j])
+                elif i.endswith('label.nrrd'):
+                    filepath = os.path.join(path, i)
+                    data, nrrd_options = nrrd.read(filepath)
+                    print(i, data.shape)
+                    data=np.reshape(data,(data.size,))
+                    print(np.unique(data))
+                    print('*'*90)
+                    # for j in nrrd_options:
+                    #     print(j, nrrd_options[j])
+
 if __name__=='__main__':
-    d=data_loader('D:\\to-lishuai')
+    dir='C:\\Users\chris.li2\\3D_medical'
+    d=data_loader(dir)
     d.process()
+
---
+++ model/UNet.py
@@ -0,0 +1,12 @@
+import numpy as np
+from functools import partial
+from keras import backend as K
+from keras.engine import Input,Model
+from keras.layers import Conv3D,MaxPooling3D,UpSampling3D,Activation
+from keras.layers import BatchNormalization,PReLU,Deconvolution3D
+from keras.layers.merge import concatenate
+from keras.optimizers import Adam,SGD,RMSprop
+
+class UNet(object):
+    def __init__(self):
+        pass
